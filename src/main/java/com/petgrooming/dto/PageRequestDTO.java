8f60804136f830b909f9e2f5f832f9e75dd9413e
xodzh234 <xodzh234@naver.com>
2024-03-15 13:23:58 +0900

    황 첫번째

--- src/main/java/com/petgrooming/dto/PageRequestDTO.java
+++ src/main/java/com/petgrooming/dto/PageRequestDTO.java
@@ -5,21 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.domain.Sort;
 
 @Data
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class PageRequestDTO {
-
-    @Builder.Default
-    private int page = 1;
-
-    @Builder.Default
-    private int size = 10;
-
-    @Builder.Default
-    private Sort.Direction sortDirection = Sort.Direction.ASC;  // 정렬 방향 필드 추가
-
+	@Builder.Default
+	private int page = 1;
+	
+	@Builder.Default
+	private int size = 10;
 }
---
+++ src/main/java/com/petgrooming/service/DesignerService.java
@@ -0,0 +1,21 @@
+package com.petgrooming.service;
+
+import com.petgrooming.dto.DesignerDTO;
+import com.petgrooming.dto.PageRequestDTO;
+import com.petgrooming.dto.PageResponseDTO;
+import com.petgrooming.repository.DesignerRepository;
+
+public interface DesignerService { 
+	
+	//목록데이터처리, PageRequestDTO 파라미터 처리, PageResponseDTO 타입 리턴타입지정
+	PageResponseDTO<DesignerDTO> getList(PageRequestDTO pageRequestDTO);
+	//등록기능선언
+	Long register(DesignerDTO designerDTO);
+	//서비스 조회	
+	DesignerDTO get(Long dno);
+	//수정기능
+	void modify(DesignerDTO designerDTO); 
+	
+	//삭제기능
+	void remove(Long dno); 
+}
