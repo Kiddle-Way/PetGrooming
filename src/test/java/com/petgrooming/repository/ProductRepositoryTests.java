cbe3f63f6a6b1b9c81f08cc6534cac34a3b5a23e
ssunmyung97 <smkim0922@naver.com>
2024-04-03 14:53:00 +0900

    last sun

--- src/test/java/com/petgrooming/repository/ProductRepositoryTests.java
+++ src/test/java/com/petgrooming/repository/ProductRepositoryTests.java
@@ -23,39 +23,39 @@ public class ProductRepositoryTests {
 		productRepository.save(product);
 		Product product1 = Product.builder().p_type("추가상품").p_name("스파").p_price(20000).build();
 		productRepository.save(product1);
-		Product product2 = Product.builder().p_type("필수상품").p_name("01 목욕 ~4kg").p_price(20000).build();
+		Product product2 = Product.builder().p_type("필수상품").p_name("01 목욕 ~4kg").p_price(25000).build();
 		productRepository.save(product2);
-		Product product3 = Product.builder().p_type("필수상품").p_name("02 목욕 5~8kg").p_price(30000).build();
+		Product product3 = Product.builder().p_type("필수상품").p_name("02 목욕 5~8kg").p_price(35000).build();
 		productRepository.save(product3);
-		Product product4 = Product.builder().p_type("필수상품").p_name("03 목욕 9~12kg").p_price(40000).build();
+		Product product4 = Product.builder().p_type("필수상품").p_name("03 목욕 9~12kg").p_price(45000).build();
 		productRepository.save(product4);
-		Product product5 = Product.builder().p_type("필수상품").p_name("04 목욕 13~15kg").p_price(50000).build();
+		Product product5 = Product.builder().p_type("필수상품").p_name("04 목욕 13~15kg").p_price(55000).build();
 		productRepository.save(product5);
 		Product product6 = Product.builder().p_type("필수상품").p_name("01 위생 ~4kg").p_price(15000).build();
 		productRepository.save(product6);
-		Product product7 = Product.builder().p_type("필수상품").p_name("02 위생 5~8kg").p_price(20000).build();
+		Product product7 = Product.builder().p_type("필수상품").p_name("02 위생 5~8kg").p_price(25000).build();
 		productRepository.save(product7);
 		Product product8 = Product.builder().p_type("필수상품").p_name("03 위생 9~12kg").p_price(35000).build();
 		productRepository.save(product8);
-		Product product9 = Product.builder().p_type("필수상품").p_name("04 위생 13~15kg").p_price(40000).build();
+		Product product9 = Product.builder().p_type("필수상품").p_name("04 위생 13~15kg").p_price(45000).build();
 		productRepository.save(product9);
-		Product product10 = Product.builder().p_type("필수상품").p_name("01 목욕➕위생 ~4kg").p_price(30000).build();
+		Product product10 = Product.builder().p_type("필수상품").p_name("01 목욕➕위생 ~4kg").p_price(35000).build();
 		productRepository.save(product10);
-		Product product11 = Product.builder().p_type("필수상품").p_name("02 목욕➕위생 5~8kg").p_price(45000).build();
+		Product product11 = Product.builder().p_type("필수상품").p_name("02 목욕➕위생 5~8kg").p_price(50000).build();
 		productRepository.save(product11);
-		Product product12 = Product.builder().p_type("필수상품").p_name("03 목욕➕위생 9~12kg").p_price(70000).build();
+		Product product12 = Product.builder().p_type("필수상품").p_name("03 목욕➕위생 9~12kg").p_price(65000).build();
 		productRepository.save(product12);
-		Product product13 = Product.builder().p_type("필수상품").p_name("04 목욕➕위생 13~15kg").p_price(85000).build();
+		Product product13 = Product.builder().p_type("필수상품").p_name("04 목욕➕위생 13~15kg").p_price(80000).build();
 		productRepository.save(product13);
-		Product product14 = Product.builder().p_type("필수상품").p_name("01 클리핑 ~4kg").p_price(20000).build();
+		Product product14 = Product.builder().p_type("필수상품").p_name("01 클리핑 ~4kg").p_price(45000).build();
 		productRepository.save(product14);
-		Product product15 = Product.builder().p_type("필수상품").p_name("02 클리핑 5~8kg").p_price(30000).build();
+		Product product15 = Product.builder().p_type("필수상품").p_name("02 클리핑 5~8kg").p_price(60000).build();
 		productRepository.save(product15);
-		Product product16 = Product.builder().p_type("필수상품").p_name("03 클리핑 9~12kg").p_price(40000).build();
+		Product product16 = Product.builder().p_type("필수상품").p_name("03 클리핑 9~12kg").p_price(75000).build();
 		productRepository.save(product16);
-		Product product17 = Product.builder().p_type("필수상품").p_name("04 클리핑 13~15kg").p_price(50000).build();
+		Product product17 = Product.builder().p_type("필수상품").p_name("04 클리핑 13~15kg").p_price(90000).build();
 		productRepository.save(product17);
-		Product product18 = Product.builder().p_type("필수상품").p_name("01 가위컷 ~4kg").p_price(75000).build();
+		Product product18 = Product.builder().p_type("필수상품").p_name("01 가위컷 ~4kg").p_price(80000).build();
 		productRepository.save(product18);
 		Product product19 = Product.builder().p_type("필수상품").p_name("02 가위컷 5~8kg").p_price(90000).build();
 		productRepository.save(product19);
@@ -72,32 +72,4 @@ public class ProductRepositoryTests {
 		Product product25 = Product.builder().p_type("필수상품").p_name("04 스포팅 13~15kg").p_price(120000).build();
 		productRepository.save(product25);
 	}
-
-	/*
-	 * // 데이터 조회 테스트
-	 * 
-	 * @Test public void testRead() { Long p_num = 1L; java.util.Optional<Product>
-	 * result = productRepository.findById(p_num); Product product =
-	 * result.orElseThrow(); log.info(product); }
-	 * 
-	 * // 데이터 수정 테스트
-	 * 
-	 * @Test public void testModify() { Long p_num = 1L; java.util.Optional<Product>
-	 * result = productRepository.findById(p_num); Product product =
-	 * result.orElseThrow(); product.changeP_type("추가상품");
-	 * product.changeP_name("스파"); product.changeP_price(10000);
-	 * productRepository.save(product); }
-	 * 
-	 * // 데이터 삭제 테스트
-	 * 
-	 * @Test public void testDelete() { Long p_num = 21L;
-	 * productRepository.deleteById(p_num); }
-	 */
-
-	/*
-	 * @Test public void testPaging() { Pageable pageable = PageRequest.of(0, 10);
-	 * Page<Product> result = productRepository.findAllOrderByPnumDesc(pageable);
-	 * log.info(result.getTotalElements());
-	 * result.getContent().stream().forEach(product -> log.info(product)); }
-	 */
 }
